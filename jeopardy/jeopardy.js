2ae51632e04a69281df068f6a43ad051cf078cb0
Michael Healy <michael.healy30@yahoo.com>
2024-03-11 22:00:14 -0400

    Submission Version

--- jeopardy/jeopardy.js
+++ jeopardy/jeopardy.js
@@ -1,7 +1,10 @@
 let categoryIds = [2, 3, 4, 6, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18];
 let baseUrl = 'https://rithm-jeopardy.herokuapp.com/api';
-let shuffledCategoryIds = getCategoryIds();
+const startButton = document.querySelector('button');
 
+startButton.addEventListener('click', setupAndStart);
+
+// shuffles the Ids of the categories
 function shuffleCategoryIds() {
 	for (let i = categoryIds.length - 1; i > 0; i--) {
 		const randomIndex = Math.floor(Math.random() * (i + 1));
@@ -9,52 +12,35 @@ function shuffleCategoryIds() {
 	}
 }
 
-/** Get NUM_CATEGORIES random category from API.
- *
- * Returns array of category ids
- */
-
+// Returns an array of 6 categories
 function getCategoryIds() {
 	shuffleCategoryIds();
 	return categoryIds.slice(0, 6);
 }
 
-/** Return object with data about a category:
- *
- *  Returns { title: "Math", clues: clue-array }
- *
- * Where clue-array is:
- *   [
- *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
- *      {question: "Bell Jar Author", answer: "Plath", showing: null},
- *      ...
- *   ]
- */
-
+// Return object with data about a category
 async function getCategory(catId) {
 	return await axios.get(`${baseUrl}/category?id=${catId}`);
 }
 
-/** Fill the HTML table#jeopardy with the categories & cells for questions.
- *
- * - The <thead> should be filled w/a <tr>, and a <td> for each category
- * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
- *   each with a question for each category in a <td>
- *   (initally, just show a "?" where the question/answer would go.)
- */
-
+// Creates HTML table elements and adds appropriate classes; IDs used for tracking which category goes in which column and td
 function createTable() {
 	const table = document.createElement('table');
 	const thead = document.createElement('thead');
 	const tbody = document.createElement('tbody');
 	const headRow = document.createElement('tr');
+	table.classList.add('hidden');
 	headRow.classList.add('table-head');
 	thead.append(headRow);
+
+	// Creates row containing titles of categories
 	for (let i = 0; i < 6; i++) {
 		const td = document.createElement('td');
 		td.setAttribute('id', `${i}`);
 		headRow.append(td);
 	}
+
+	// Creates table body for questions/answers
 	for (let i = 0; i < 5; i++) {
 		const tr = document.createElement('tr');
 		tr.classList.add('table-body');
@@ -75,7 +61,9 @@ function createTable() {
 	$('body').append(table);
 }
 
+// Fills already created HTML table with info from API
 async function fillTable() {
+	let shuffledCategoryIds = getCategoryIds();
 	const headRow = document.querySelector('tr.table-head');
 	const columnHeaders = headRow.querySelectorAll('td');
 	for (let column of columnHeaders) {
@@ -84,12 +72,14 @@ async function fillTable() {
 		const category = await axios.get(`${baseUrl}/category?id=${categoryId}`);
 		const categoryTitle = category.data.title;
 		columnTitle.innerText = categoryTitle;
-		column.append(columnTitle)
+		column.append(columnTitle);
 	}
-	
+
 	const allBodyRows = document.querySelectorAll('tr.table-body');
 	for (let i = 0; i < allBodyRows.length; i++) {
 		const tdsInRow = allBodyRows[i].querySelectorAll('td');
+
+		// Adds p elements with corresponding classes to differentiate between question and answer when clicking elements
 		for (let td of tdsInRow) {
 			const questionP = document.createElement('p');
 			questionP.classList.add('question');
@@ -109,9 +99,10 @@ async function fillTable() {
 			answerP.innerText = answer;
 		}
 	}
-	
+	hideLoadingView();
 }
 
+// Handles clicking on a clue: show the question or answer based on element class
 function toggleQuestion(evt) {
 	if (evt.target.classList.contains('very-large')) {
 		evt.target.classList.remove('very-large');
@@ -127,43 +118,44 @@ function toggleQuestion(evt) {
 	}
 }
 
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
-
-function handleClick(evt) {}
-
-/** Wipe the current Jeopardy board, show the loading spinner,
- * and update the button used to fetch data.
- */
-
-function showLoadingView() {}
-
-/** Remove the loading spinner and update the button used to fetch data. */
-
-function hideLoadingView() {}
-
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
-createTable();
-fillTable();
-async function setupAndStart() {
-	createTable();
-	fillTable();
+// Hides start button, creates and shows loading spinner
+function showLoadingView() {
+	startButton.classList.add('hidden');
+	loadingSpinner = document.createElement('i');
+	loadingSpinner.classList.add('fa-solid', 'fa-spinner', 'fa-spin-pulse');
+	$('body').append(loadingSpinner);
 }
 
-/** On click of start / restart button, set up game. */
+// Removes loading spinner and shows restart button
+function hideLoadingView() {
+	loadingSpinner.style.display = 'none';
+	$('table').removeClass('hidden');
+	startButton.classList.remove('hidden', 'start');
+	startButton.classList.add('restart');
+	startButton.innerText = 'Restart';
+}
 
-// TODO
+// Starts game by calling necessary functions
+async function setupAndStart(evt) {
+	try {
+		// If the restart button triggered the event, empties page except for button and adds new event listener
+		if (document.querySelector('table')) {
+			document.querySelector('body').innerHTML = '<button class="restart">Restart</button>';
+			document.querySelector('button').addEventListener('click', setupAndStart);
+		}
+		showLoadingView();
+		createTable();
+		await fillTable();
+	} catch (error) {
+		showError();
+	}
+}
 
-/** On page load, add event handler for clicking clues */
-
-// TODO
+function showError() {
+	const errorMessage = document.createElement('h1');
+	errorMessage.classList.add('error');
+	errorMessage.innerText = 'An error occurred while processing your request';
+	$('body').empty()
+	document.querySelector('body').append(errorMessage);
+	
+}
